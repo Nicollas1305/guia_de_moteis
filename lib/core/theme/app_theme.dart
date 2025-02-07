cd0ee6e5d3903047291794525acd8140f56951d7
Nicollas1305 <cartola1320@gmail.com>
2025-02-07 11:20:47 -0300

    feat: Add theme Data.

---
+++ lib/core/theme/app_theme.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+
+class AppTheme {
+  static ThemeData get themeData {
+    return ThemeData(
+      useMaterial3: false,
+      primaryColor: const Color(0xFFD01721),
+      secondaryHeaderColor: const Color(0xFFF8F8FA),
+      disabledColor: const Color(0xFFB9000D),
+      scaffoldBackgroundColor: const Color(0xFFF9F9FA),
+    );
+  }
+}
