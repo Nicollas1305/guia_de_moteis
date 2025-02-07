98fdd2521088a2116b08a8e8cc393d725e698fd8
Nicollas1305 <cartola1320@gmail.com>
2025-02-07 20:49:01 -0300

    feat: Add default color to progress Indicator Theme.

--- lib/core/theme/app_theme.dart
+++ lib/core/theme/app_theme.dart
@@ -8,6 +8,9 @@ class AppTheme {
       secondaryHeaderColor: const Color(0xFFF8F8FA),
       disabledColor: const Color(0xFFB9000D),
       scaffoldBackgroundColor: const Color(0xFFF9F9FA),
+      progressIndicatorTheme: const ProgressIndicatorThemeData(
+        color: Color(0xFFD01721),
+      ),
     );
   }
 }
